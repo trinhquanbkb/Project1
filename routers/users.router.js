f385eadd4eb844c42b54195724f209c01ef2bf35
trinhquanbkb <quan20028a3kb@gmail.com>
2022-11-11 11:14:45 +0700

    branch-3: forgotPassword, changePassword

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const userRouter = express.Router()
-const { register, login } = require('../controller/user.controller')
+const { register, login, forgotPassword, changePassword } = require('../controller/user.controller')
+const { authenticate } = require('../middleware/authentication')
+const { adminAuthorize, userAuthorize, userOtherSchoolAuthorize } = require('../middleware/authorization')
 
 userRouter.post('/register', register)
 userRouter.post('/login', login)
+userRouter.post('/forgotPassword', forgotPassword)
+userRouter.post('/changePassword', authenticate, userAuthorize, changePassword)
 
 module.exports = {
     userRouter

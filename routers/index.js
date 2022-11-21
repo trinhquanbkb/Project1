9280425fc35b1096448a6d55c0a6bfa29ae6605b
Quang <thanhquangnguyenhien@gmail.com>
2022-11-21 23:50:37 +0700

    branch-7:find By Id of card student

--- routers/index.js
+++ routers/index.js
@@ -3,10 +3,12 @@ const rootRouter = express.Router()
 const { bookRouter } = require('./books.router')
 const { userRouter } = require('./users.router')
 const { placesRouter } = require('./places.router')
+const { cardStudentRouter } = require('./cardStudents.router')
 
 rootRouter.use('/books', bookRouter)
 rootRouter.use('/users', userRouter)
 rootRouter.use('/places', placesRouter)
+rootRouter.use('/cardStudents', cardStudentRouter)
 
 module.exports = {
     rootRouter

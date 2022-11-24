5977f77413e8a21a6ba86d10b3512fd2b19f70a3
trinhquanbkb <103089390+trinhquanbkb@users.noreply.github.com>
2022-11-24 12:41:16 +0700

    Revert "Revert "branch-7: CRUD card""

--- BackEnd/routers/cardStudents.router.js
+++ BackEnd/routers/cardStudents.router.js
@@ -2,12 +2,16 @@ const express = require('express')
 const cardStudentRouter = express.Router()
 const { authenticate } = require('../middleware/authentication')
 const { adminAuthorize, userAuthorize, userOtherSchoolAuthorize, allAuthorize } = require('../middleware/authorization')
-const { findById } = require('../controller/cardStudents.controller')
+const { findById, getAllCard, createCard, updateCard, deleteCard } = require('../controller/cardStudents.controller')
 const { checkCardStudents } = require('../middleware/validation')
 
 cardStudentRouter.get('/:id', authenticate, userAuthorize, checkCardStudents, findById)
+cardStudentRouter.get('/', authenticate, userAuthorize, getAllCard)
+cardStudentRouter.post('/', authenticate, adminAuthorize, createCard)
+cardStudentRouter.put('/:id', authenticate, adminAuthorize, checkCardStudents, updateCard)
+cardStudentRouter.delete('/:id', authenticate, adminAuthorize, checkCardStudents, deleteCard)
 
 module.exports = {
-    cardStudentRouter
+    cardStudentRouter,
 }
 

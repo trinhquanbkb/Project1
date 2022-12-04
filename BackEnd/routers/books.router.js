89a7aca5435c0f16e8fcac2b69e3a36168cb58ea
Quang <thanhquangnguyenhien@gmail.com>
2022-12-04 12:48:34 +0700

    branch-12: Extend Book

--- BackEnd/routers/books.router.js
+++ BackEnd/routers/books.router.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const bookRouter = express.Router()
 const { checkBook } = require('../middleware/validation')
-const {createBook, deleteBook, getAllBook, getBookById, updateBook} = require('../controller/books.controller')
+const {createBook, deleteBook, getAllBook, getBookById, updateBook,extendBook} = require('../controller/books.controller')
 const { authenticate } = require('../middleware/authentication')
-const { adminAuthorize, userAuthorize, userOtherSchoolAuthorize } = require('../middleware/authorization')
+const { adminAuthorize, userAuthorize, userOtherSchoolAuthorize, allAuthorize } = require('../middleware/authorization')
 
 bookRouter.post('/', authenticate, adminAuthorize, createBook)
 bookRouter.delete('/:id', authenticate, adminAuthorize, checkBook, deleteBook)
 bookRouter.get('/', authenticate, userAuthorize, getAllBook)
 bookRouter.get('/:id', authenticate, userAuthorize, checkBook, getBookById)
 bookRouter.put('/:id', authenticate, adminAuthorize, updateBook)
+bookRouter.put('/extendBook',authenticate,userAuthorize,extendBook)
 
 module.exports = { 
     bookRouter,

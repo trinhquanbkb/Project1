7fbbe2b25295c7ca523c1a330b9f93a1e9a415b6
Quang <thanhquangnguyenhien@gmail.com>
2022-12-04 14:38:26 +0700

    branch-12: Extend Book

--- BackEnd/routers/books.router.js
+++ BackEnd/routers/books.router.js
@@ -10,7 +10,7 @@ bookRouter.delete('/:id', authenticate, adminAuthorize, checkBook, deleteBook)
 bookRouter.get('/', authenticate, userAuthorize, getAllBook)
 bookRouter.get('/:id', authenticate, userAuthorize, checkBook, getBookById)
 bookRouter.put('/:id', authenticate, adminAuthorize, updateBook)
-bookRouter.put('/extendBook',authenticate,userAuthorize,extendBook)
+bookRouter.put('/extendBook/:id',authenticate,userAuthorize,extendBook)
 
 module.exports = { 
     bookRouter,

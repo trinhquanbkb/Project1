1cc6288b6322b7dfa4eaf8ba0edd5a45c35d7d64
trinhquanbkb <quan20028a3kb@gmail.com>
2022-12-08 13:52:46 +0700

    branch-13: min Time

--- BackEnd/routers/books.router.js
+++ BackEnd/routers/books.router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bookRouter = express.Router()
 const { checkBook } = require('../middleware/validation')
-const {createBook, deleteBook, getAllBook, getBookById, updateBook,extendBook,borrowBook, giveBook, totalBook, unborrowListBook, historyBookBorrowOfStudent, listUser} = require('../controller/books.controller')
+const {createBook, deleteBook, getAllBook, getBookById, updateBook,extendBook,borrowBook, giveBook, totalBook, unborrowListBook, historyBookBorrowOfStudent, listUser, minTime} = require('../controller/books.controller')
 const { authenticate } = require('../middleware/authentication')
 const { adminAuthorize, userAuthorize, userOtherSchoolAuthorize, allAuthorize } = require('../middleware/authorization')
 
@@ -17,7 +17,7 @@ bookRouter.get('/totalBook/:name', authenticate, allAuthorize, totalBook)
 bookRouter.get('/unborrowListBook', authenticate, allAuthorize,unborrowListBook)
 bookRouter.get('/historybookborrowofstudent/:userId', authenticate, userAuthorize, historyBookBorrowOfStudent)
 bookRouter.get('/listUser',authenticate, adminAuthorize, listUser)
-
+bookRouter.get('/minTime/:name', authenticate, userAuthorize,minTime)
 
 module.exports = { 
     bookRouter,
